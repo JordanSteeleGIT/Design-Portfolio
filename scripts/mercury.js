92f1684f8fe46a5ac39b4f15dd5ee0fb25793b7a
JordanSteeleGIT <39620858+JordanSteele96@users.noreply.github.com>
2021-05-19 16:53:39 +0100

    Custom video player
    Added a custom video player. This needs to be refactored at a later dated!

--- scripts/mercury.js
+++ scripts/mercury.js
@@ -1,5 +1,44 @@
 var container = document.querySelector(".side-Nav-Wrapper");
 
+var videoOne = document.querySelector("#video1");
+var videoTwo = document.querySelector("#video2");
+var videoThree = document.querySelector("#video3");
+
+var btnOne = document.querySelector(".button-one");
+var btnTwo = document.querySelector(".button-two");
+var btnThree = document.querySelector(".button-three");
+
+//For shame. This needs to be done properly next time.
+function togglePlayPauseVideoOne() {
+  if (videoOne.paused) {
+    btnOne.className = "pause";
+    videoOne.play();
+  } else {
+    btnOne.className = "play";
+    videoOne.pause();
+  }
+}
+
+function togglePlayPauseVideoTwo() {
+  if (videoTwo.paused) {
+    btnTwo.className = "pause";
+    videoTwo.play();
+  } else {
+    btnTwo.className = "play";
+    videoTwo.pause();
+  }
+}
+
+function togglePlayPauseVideoThree() {
+  if (videoThree.paused) {
+    btnThree.className = "pause";
+    videoThree.play();
+  } else {
+    btnThree.className = "play";
+    videoThree.pause();
+  }
+}
+
 window.onscroll = function () {
   stickyNav();
 };
