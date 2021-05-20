948431163786626d486bbc2d7fb99f7034e07e44
JordanSteeleGIT <39620858+JordanSteele96@users.noreply.github.com>
2021-05-20 18:50:28 +0100

    Added slick sliders
    -Added two slick sliders
    -Changed the play/pause icons

---
+++ scripts/slick-config.js
@@ -0,0 +1,16 @@
+$(".brands-Image-Container").slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+});
+
+$(".user-Research-Right").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: false,
+  nextArrow: false,
+  dots: true,
+});
