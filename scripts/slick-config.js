0e8debc4a381b29c2f09bf3a824268fb7ab5e96e
JordanSteeleGIT <39620858+JordanSteele96@users.noreply.github.com>
2021-05-21 15:36:35 +0100

    More slick sliders
    -Added more slick sliders to mercury page
    -Added 3 new containers and a footer to the page

--- scripts/slick-config.js
+++ scripts/slick-config.js
@@ -1,11 +1,16 @@
+//Homepage banner slider
 $(".brands-Image-Container").slick({
   dots: false,
   prevArrow: false,
   nextArrow: false,
   slidesToShow: 2,
-  slidesToScroll: 1,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 800,
 });
 
+//Mercury user research slider
 $(".user-Research-Right").slick({
   infinite: true,
   slidesToShow: 1,
@@ -13,4 +18,54 @@ $(".user-Research-Right").slick({
   prevArrow: false,
   nextArrow: false,
   dots: true,
+});
+
+//Final iteration slider
+$(".final-Interation-Slider").slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  // speed: 800,
+});
+
+//Booking changing room slider
+$(".booking-Slider-Container").slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 4,
+  slidesToScroll: 3,
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  // speed: 800,
+});
+
+//Finding a store
+
+$(".store-Slider-Container").slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 4,
+  slidesToScroll: 3,
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  // speed: 800,
+});
+
+//Ordering items slider
+
+$(".order-Slider-Container").slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 4,
+  slidesToScroll: 3,
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  // speed: 800,
 });
